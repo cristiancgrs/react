3d86482d44348a1813f1699479367b65379d952c
cgrs <cristiancgrs@gmail.com>
2017-03-09 20:11:48 -0430

    pruebas de commit

--- app/src/components/FilterableProductTable.js
+++ app/src/components/FilterableProductTable.js
@@ -10,13 +10,24 @@ import ProductTable from './ProductTable.js'
 export default class FilterableProductTable extends React.Component{
     constructor(){
         super()
+        this.state = {
+            filer : null
+        }
+    }
+
+    filterList(ev){
+        let filter = ev.target.value
+
+        this.setState({
+            filter: filter
+        })
     }
 
     render(){
         return(
             <div>
-                <SearchBar/>
-                <ProductTable products={this.props.store}/>
+                <SearchBar onChange={this.filterList.bind(this)} />
+                <ProductTable products={this.props.store} filter={this.state.filter}/>
             </div>
         )
     }
